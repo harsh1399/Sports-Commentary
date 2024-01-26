97c7708877259cb1ef106d8d43a907aa1eb580bb
harsh1399 <harsh321mahajan@gmail.com>
2024-01-26 11:10:14 -0700

    slurm script

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-user=u1413898@umail.utah.edu
 #SBATCH --mail-type=FAIL,END
-#SBATCH -o assignment_5-%j
+#SBATCH -o project-%j
 #SBATCH --export=ALL
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate nlp
 OUT_DIR=/scratch/general/vast/u1413898/cs6957/project/logs
 mkdir -p ${OUT_DIR}
-python transformer.py --output_dir ${OUT_DIR}
+python model.py --output_dir ${OUT_DIR}
