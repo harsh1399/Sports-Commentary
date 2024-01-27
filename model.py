f2ef603f9de26816279650e04cb744f3bd9b3026
harsh1399 <harsh321mahajan@gmail.com>
2024-01-26 17:04:40 -0700

    inference debugging

--- model.py
+++ model.py
@@ -60,7 +60,7 @@ def train_model(output_dir):
 
 def inference(test_dataset,output_dir):
     for idx in range(len(test_dataset)):
-        # print(test_dataset[idx])
+        print(test_dataset[idx]['pixel_values'].shape)
         generated_commentary = utils.tokenizer.decode(model.generate(test_dataset[idx]['pixel_values'])[0])
         with open(f"{output_dir}/test.txt",'a') as f:
             f.write(generated_commentary+"\n")
